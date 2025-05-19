b054f8e35603deb8b30767ba8e3fbb9feef7ebaa
chenjw <996204708@qq.com>
2025-05-19 19:03:48 +0800

    feat(setting): 添加设置页面主布局

--- app/src/main/java/io/jarvis/pma/SettingActivity.kt
+++ app/src/main/java/io/jarvis/pma/SettingActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import io.jarvis.pma.ui.theme.ProtectMyAppTheme
 
 class SettingActivity : ComponentActivity() {
@@ -12,8 +17,17 @@ class SettingActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ProtectMyAppTheme {
-
+                SettingMainLayout()
             }
+        }
+    }
+}
+
+@Composable
+fun SettingMainLayout() {
+    Scaffold() { padding ->
+        Box(Modifier.padding(padding)) {
+
         }
     }
 }
